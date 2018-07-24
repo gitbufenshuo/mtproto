6ad037194a052e7d94ebd6c7f2eec5fef08fe6ec
王思源 <sywang@firewinggames.com>
2018-07-24 12:54:44 +0800

    add wechatid

--- mtproto.go
+++ mtproto.go
@@ -297,7 +297,7 @@ func (m *MTProto) InvokeSync(msg TL) (*TL, error) {
 		if err, ok := x.err.(TL_rpc_error); ok {
 			switch err.Error_code {
 			case errorSeeOther:
-				fmt.Println("m.errorSeeOther---")
+				fmt.Println("m.errorSeeOther---", err.Error_message)
 				var newDc int32
 				n, _ := fmt.Sscanf(err.Error_message, "PHONE_MIGRATE_%d", &newDc)
 				if n != 1 {
